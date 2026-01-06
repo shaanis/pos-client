e4f5a09fd20f3a51f1983ecad1caa049c0b5a619
shaanis <mhdshanis3@gmail.com>
2026-01-06 19:49:40 +0530

    updated url

--- src/services/serverurl.ts
+++ src/services/serverurl.ts
@@ -1,3 +1,4 @@
 // src/services/serverurl.ts
-const serverurl = "https://t3lrs903-3000.inc1.devtunnels.ms"  ; // update as per your backend
+// const serverurl = "https://t3lrs903-3000.inc1.devtunnels.ms"  ; 
+const serverurl = "https://pos-server-e51q.onrender.com"  ; 
 export default serverurl;
