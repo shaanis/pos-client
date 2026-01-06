09168dafd9de7bf56b7cd1c40dce5e62f410dbd8
shaanis <mhdshanis3@gmail.com>
2026-01-07 03:25:03 +0530

    new severurl fixed

--- src/components/FoodGrid.tsx
+++ src/components/FoodGrid.tsx
@@ -35,7 +35,7 @@ const FoodGrid = ({ sidebarExpanded }: Props) => {
     return (
       <div className="flex justify-center items-center h-full text-gray-400 gap-2">
         <Loader2 className="animate-spin w-6 h-6" />
-        <span>Loading meals...</span>
+        <span>Loading products...</span>
       </div>
     );
   }
